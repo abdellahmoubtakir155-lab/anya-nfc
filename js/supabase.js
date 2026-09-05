708a6331c21106edc124b2a09942836fd65a209b
abdellahmoubtakir155-lab <abdellahmoubtakir155@gmail.com>
2026-09-05 21:05:19 +0100

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,9 @@
+const SUPABASE_URL = "ضع_هنا_Project_URL";
+const SUPABASE_KEY = "ضع_هنا_Publishable_Key";
+
+const supabaseClient =
+  supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+  );<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
+<script src="js/supabase.js"></script>
